b674042039dfdd2df531f2397cf11de2a4fa3180
TuNguyen229VN <insec229@gmail.com>
2026-06-04 16:18:48 +0700

    fix: fix ui

--- src/modules/combo/ComboSelector.js
+++ src/modules/combo/ComboSelector.js
@@ -381,7 +381,7 @@ export default function ComboSelector({
                     }
                 </div>
                 {validationError && (
-                    <div className="px-3 py-2 text-xs text-red-600 border border-red-200 rounded-lg bg-red-50">
+                    <div className="px-3 py-2 mt-4 text-xs text-red-600 border border-red-200 rounded-lg bg-red-50">
                         {validationError}
                     </div>
                 )}
